5f6b813c3b60916102155d3871a0f33cfc88d413
unknown <trabajodam18@gmail.com>
2019-10-27 21:46:11 +0100

    Estilos copn XAML y WPF

---
+++ Estylos/Principal.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Estylos
+{
+    /// <summary>
+    /// Lógica de interacción para Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EstiloApp_Click(object sender, RoutedEventArgs e)
+        {
+            new EstiloAppXaml().Show();
+        }
+
+        private void EstiloExterno_Click(object sender, RoutedEventArgs e)
+        {
+            new VentanaEstiloExterno().Show();
+        }
+
+        private void MaterialD_Click(object sender, RoutedEventArgs e)
+        {
+            new MaterialD().Show();
+        }
+    }
+}
